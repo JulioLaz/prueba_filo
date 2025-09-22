00e5b7e6101a91ab0885b31f383ae0d4bf61dc43
JulioLaz <julioalazarte@gmail.com>
2025-09-22 11:17:00 -0300

    temaChip

--- js/educational-modal.js
+++ js/educational-modal.js
@@ -55,7 +55,7 @@ console.log('📚 Cargando sistema de modal educativo...');
           </div>
           
           <div class="modal-actions">
-            <button class="btn-exit" onclick="EducationalModal.exit()" >
+            <button class="btn-exit" onclick="EducationalModal.exit()" style="display: none;" >
               Actividades
             </button>
             <button class="btn-secondary" onclick="EducationalModal.skip()" style="display:none">
